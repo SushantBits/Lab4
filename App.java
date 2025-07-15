7062a7b99cb33de5e24fb3b27d1184b150b73884
SushantBits <2021wb86977@wilp.bits-pilani.ac.in>
2025-07-15 21:14:09 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 package com.example;
 public class App {
   public static void main(String[] args) {
-    System.out.println("CI/CD Works!");
+    System.out.println("Autotriggering check");
   }
 }
